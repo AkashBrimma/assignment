e0b133a8cb228fb18c5c69f2b9865ab53f2ace18
Lvarathan <110367832+Lvarathan@users.noreply.github.com>
2022-08-02 15:50:52 +0530

    Update special pythagorean triple.c

--- special pythagorean triple.c
+++ special pythagorean triple.c
@@ -1,4 +1,4 @@
-/*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+/*9.A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 a2 + b2 = c2
 For example, 32 + 42 = 9 + 16 = 25 = 52.
