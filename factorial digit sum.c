10037fd60e6635441b17b7f0dc45a139fbd34aaa
Lvarathan <110367832+Lvarathan@users.noreply.github.com>
2022-08-02 16:09:48 +0530

    Update factorial digit sum.c

--- factorial digit sum.c
+++ factorial digit sum.c
@@ -4,23 +4,31 @@ and the sum of the digits in the number 10! is
 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
 Ans: 648*/
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include"big_int.h"
-
-int main(int argc,const char* argv[])
-{
-    BigInt N(1);
-    int32_t i;
-    uint32_t sum = 0;
-    
-    for (i=2; i<=100; ++i) {
-        N.Multiply(i);
+#include <stdio.h>
+#define MAX 500
+int main(){
+int vet[MAX]={0};
+int i,x,y,v,f,sum;
+vet[0]=1;
+y=2;
+while (y<101){
+  v = 0;
+  for (i=0;i<MAX;i++){
+    x = vet[i]*y + v;
+    v = 0;
+    if (x > 9){
+      f = x % 10;
+      v = x / 10;      
     }
-    while (!N.Zero()) {
-        sum += N.DivMod(10);
-    }
-    printf("sum = %u\n", sum);
-    return 0;
+    else
+      f = x;
+    vet[i] = f;
+  }
+  y++;
+}
+sum=0;
+for (i=MAX-1;i>=0;i--)
+  sum=sum+vet[i];
+printf("The sum of the digits in the number 100!:%d",sum);
+return 0;
 }
